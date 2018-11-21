9c64257bc40d619964d6cc8d3cfaa3d2b6f87b62
Rob Anderson <rob.anderson@ncl.ac.uk>
2018-11-21 17:10:57 +0000

    :star: Start on ShowPoster

--- src/views/ShowPoster.vue
+++ src/views/ShowPoster.vue
@@ -18,6 +18,18 @@
                 li: router-link(to="/") Home
                 li: router-link(to="/posters") Posters
                 li.is-active: router-link(to="/posters/add") {{posterName}}
+  
+  section.section(v-if="poster")
+    .container
+      .columns
+        .column.is-two-fifths
+          div
+            h3.title.is-3 {{posterQuestion}}
+          ul.is-size-4
+            li(v-for="option, index in poster.options")
+              strong {{index + 1}}.
+              span  {{option.text}}
+        .column
 </template>
 
 <script>
@@ -27,6 +39,9 @@ import { ROUTE_LIST_POSTERS, MUTATION_POSTERS } from '@/const'
 
 export default {
   components: { SiteNav },
+  data: () => ({
+    votes: null
+  }),
   computed: {
     posterId() {
       return parseInt(this.$route.params.id, 10)
@@ -43,6 +58,7 @@ export default {
   },
   mounted() {
     this.fetchPoster()
+    this.fetchVotes()
   },
   methods: {
     async fetchPoster() {
@@ -52,6 +68,13 @@ export default {
       } else {
         this.$router.replace({ name: ROUTE_LIST_POSTERS })
       }
+    },
+    async fetchVotes() {
+      let { meta, data } = await sharedClient.get(
+        `posters/${this.posterId}/votes`
+      )
+      console.log(meta)
+      console.log(data)
     }
   }
 }
