d2860bf90c7a415ed1a32c353a4a63bdfbe3d9c0
Rob Anderson <rob.anderson@ncl.ac.uk>
2018-11-23 09:05:40 +0000

    :bug: Fix typo

--- src/views/ShowPoster.vue
+++ src/views/ShowPoster.vue
@@ -40,7 +40,7 @@
               button.button.is-link.is-medium(@click="printPoster")
                 | Preview &amp; print
               button.button.is-danger.is-medium(@click="destroyPoster")
-                | Archve Poster
+                | Archive Poster
         .column
           .poster-result(v-if="hasVotes")
             h2.result-title
