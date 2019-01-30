0a1fa6d125fd185089d9a9716a226187bf12822d
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-01-30 16:30:07 +0000

    :art: Shrunk use of bulma

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,37 @@ export default {
 </script>
 
 <style lang="sass">
-@import "~bulma/bulma"
+//
+// Import the bits of bulma we want to use
+//
+
+@import "~bulma/sass/base/_all"
+
+// @import "~bulma/sass/elements/_all"
+@import "~bulma/sass/elements/box"
+@import "~bulma/sass/elements/button"
+@import "~bulma/sass/elements/container"
+@import "~bulma/sass/elements/content"
+@import "~bulma/sass/elements/form"
+@import "~bulma/sass/elements/image"
+@import "~bulma/sass/elements/notification"
+@import "~bulma/sass/elements/other"
+@import "~bulma/sass/elements/progress"
+@import "~bulma/sass/elements/title"
+
+// @import "~bulma/sass/components/_all"
+@import "~bulma/sass/components/breadcrumb"
+@import "~bulma/sass/components/level"
+@import "~bulma/sass/components/message"
+@import "~bulma/sass/components/navbar"
+
+// @import "~bulma/sass/grid/_all"
+@import "~bulma/sass/grid/columns"
+
+@import "~bulma/sass/layout/_all"
+
+//
+// Import common styles for the whole app
+//
 @import "~@/assets/sass/styles"
 </style>
